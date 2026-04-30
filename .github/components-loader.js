4b7505f0f6104dd19e9aab8b99c0639c5bba0886
DELALANDE <nathan.delalande@btssio.lan>
2026-04-30 11:20:44 +0200

    test

---
+++ .github/components-loader.js
@@ -0,0 +1,9 @@
+async function loadComponent(id, file, options = {}) {
+  const res = await fetch(`components/${file}`);
+  const html = await res.text();
+  document.getElementById(id).innerHTML = html;
+}
+
+
+loadComponent('footer', 'footer.html');
+loadComponent('navbar', 'navbar.html');
